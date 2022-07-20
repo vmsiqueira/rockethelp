b0a4e9f6447d3d813e37fcd4b07bf09556da4e74
Vitor <vitormsiqueira@hotmail.com>
2022-07-20 08:45:18 -0300

    final aula 2

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { Button as NativeBaseButton, IButtonProps, Heading } from 'native-base';
+
+interface ButtonProps extends IButtonProps {
+  title: string;
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <NativeBaseButton 
+      bg="green.700"
+      h={14}
+      fontSize="sm"
+      rounded="sm"
+      _pressed={{ bg: "green.500" }}
+
+      {...rest}
+    >
+      <Heading color="white" fontSize="md" >
+        {title}
+      </Heading>
+    </NativeBaseButton>
+  );
+}
